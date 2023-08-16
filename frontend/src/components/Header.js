5504824168233a34a3f34c123f64eca02acbb165
Aracely Cruz Del ángel <yuki_oh91@hotmail.com>
2023-08-15 20:38:00 -0600

    navbar casi terminada

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,28 +1,44 @@
+import ReactDOM from "react-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+
+const element = <FontAwesomeIcon icon={faBars} beat />;
+/*
+<FontAwesomeIcon icon="fa-solid fa-bars" beat />*/
+
 function Header() {
   return (
     <>
       <div className="header">
-        <div className="navbar-container">
-          <div className="div-navbar">
-            <div className="navbar-list">
-              <a href="/" className="link-navbar">
-                Milestones
+        <nav className="nav">
+          <a href="#" className="logo nav-link">
+            Logo
+          </a>
+
+          <button className="nav__toggle">{element}</button>
+          <ul className="nav__menu">
+            <li className="nav__menu-item">
+              <a href="#" className="nav__menu-link nav-link">
+                blog
               </a>
-              <a href="/" className="link-navbar">
-                enlace hacia algo
+            </li>
+            <li className="nav__menu-item">
+              <a href="#" className="nav__menu-link nav-link">
+                videos
               </a>
-              <a href="/" className="link-navbar">
-                enlace hacia algo
+            </li>
+            <li className="nav__menu-item">
+              <a href="#" className="nav__menu-link nav-link">
+                sobre
               </a>
-              <a href="/" className="link-navbar">
-                enlace hacia algo
+            </li>
+            <li className="nav__menu-item">
+              <a href="#" className="nav__menu-link nav-link">
+                importante
               </a>
-              <a href="/" className="link-navbar">
-                enlace hacia algo
-              </a>
-            </div>
-          </div>
-        </div>
+            </li>
+          </ul>
+        </nav>
       </div>
     </>
   );
